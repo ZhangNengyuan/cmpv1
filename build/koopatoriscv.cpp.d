e1d9d84ebb864a2eae9b192e426c0286bc10e4b0
ZhangNengyuan <163854115+ZhangNengyuan@users.noreply.github.com>
2025-04-06 23:00:12 +0800

    Add files via upload

---
+++ build/koopatoriscv.cpp.d
@@ -0,0 +1,8 @@
+/root/compiler/sysy-make-template/build/koopatoriscv.cpp.o: \
+  /root/compiler/sysy-make-template/src/koopatoriscv.cpp \
+  /root/compiler/sysy-make-template/src/koopatoriscv.hpp \
+  /opt/include/koopa.h
+
+/root/compiler/sysy-make-template/src/koopatoriscv.hpp:
+
+/opt/include/koopa.h:
---
+++ build/main.cpp.d
@@ -0,0 +1,10 @@
+/root/compiler/sysy-make-template/build/main.cpp.o: \
+  /root/compiler/sysy-make-template/src/main.cpp \
+  /root/compiler/sysy-make-template/src/koopatoriscv.hpp \
+  /opt/include/koopa.h /root/compiler/sysy-make-template/src/ast.hpp
+
+/root/compiler/sysy-make-template/src/koopatoriscv.hpp:
+
+/opt/include/koopa.h:
+
+/root/compiler/sysy-make-template/src/ast.hpp:
